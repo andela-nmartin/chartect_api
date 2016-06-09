5ab4e758ee622c69f9e162490d962a78039d318f
andela-nmartin <godmetweenciati@gmail.com>
2016-06-09 18:00:36 +0300

    adjusted the function to view all the objects in database, serializer argument many set to true

--- project_charts_api/api/views/axis_data_views.py
+++ project_charts_api/api/views/axis_data_views.py
@@ -1,5 +1,5 @@
 from charts.models.axis_data import Axis_Data
-from project_charts_api.api.serializers import axis_data
+from project_charts_api.api.serializers import axis_data_serializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -17,12 +17,12 @@ class AxisData(APIView):
 
     def get(self, request, pk, format=None):
         queryset = self.get_object(pk)
-        serializer = axis_data.Axis_Data_Serializer(queryset)
+        serializer = axis_data_serializer.Axis_Data_Serializer(queryset)
         return Response(serializer.data)
 
     def put(self, request, pk, format=None):
         queryset = self.get_object(pk)
-        serializer = axis_data.Axis_Data_Serializer(queryset, data=request.data)
+        serializer = axis_data_serializer.Axis_Data_Serializer(queryset, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -32,3 +32,18 @@ class AxisData(APIView):
         queryset = self.get_object(pk)
         queryset.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class AllAxisData(APIView):
+    """
+    Getting all axis data
+    """
+    def get_object(self):
+        try:
+            return Axis_Data.objects.all()
+        except Axis_Data.DoesNotExist:
+            raise Http404
+
+    def get(self, request, format=None):
+        queryset = self.get_object()
+        serializer = axis_data_serializer.Axis_Data_Serializer(queryset, many=True)
+        return Response(serializer.data)
--- project_charts_api/api/views/axis_labels_views.py
+++ project_charts_api/api/views/axis_labels_views.py
@@ -1,5 +1,5 @@
 from charts.models.axis_labels import Axis_Labels
-from project_charts_api.api.serializers import axis_labels
+from project_charts_api.api.serializers import axis_labels_serializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -17,12 +17,12 @@ class AxisLabels(APIView):
 
     def get(self, request, pk, format=None):
         queryset = self.get_object(pk)
-        serializer = axis_labels.Axis_Labels_Serializer(queryset)
+        serializer = axis_labels_serializer.Axis_Labels_Serializer(queryset)
         return Response(serializer.data)
 
     def put(self, request, pk, format=None):
         queryset = self.get_object(pk)
-        serializer = axis_labels.Axis_Labels_Serializer(queryset, data=request.data)
+        serializer = axis_labels_serializer.Axis_Labels_Serializer(queryset, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -32,3 +32,18 @@ class AxisLabels(APIView):
         queryset = self.get_object(pk)
         queryset.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class AllAxisLabels(APIView):
+    """
+    Getting all axis data
+    """
+    def get_object(self):
+        try:
+            return Axis_Labels.objects.all()
+        except Axis_Labels.DoesNotExist:
+            raise Http404
+
+    def get(self, request, format=None):
+        queryset = self.get_object()
+        serializer = axis_labels_serializer.Axis_Labels_Serializer(queryset, many=True)
+        return Response(serializer.data)
